f7eefa9b6b79fe6e2cc5f7c2504087abd2749b7b
Aleena N A <77637415+Aleena25@users.noreply.github.com>
2021-09-04 14:49:18 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'mongodb_pointdata',         # How you named your package folder (MyLib)
-  packages = ['mongodb_pointdata'],   # Chose the same as "name"
+  name = 'point_mongo',         # How you named your package folder (MyLib)
+  packages = ['point_mongo'],   # Chose the same as "name"
   version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Library for connecting, fetching, filtering and sending back the point cloud data to MongoDB obtained using IWR6843ISK.',   # Give a short description about your library
   author = 'Aleena N A',                   # Type in your name
   author_email = 'aleenaalikhan06@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/Aleena25/Mongodb_pointdata',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Aleena25/Mongodb_pointdata/archive/refs/tags/mongodb_pointdata-v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/Aleena25/point_mongo',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Aleena25/point_mongo/archive/refs/tags/point_mongo-v_01.tar.gz',    # I explain this later on
   keywords = ['mongodb', 'pointclouddata', 'IWR6843ISK'],   # Keywords that define your package best
   # install_requires=[            # I get to this in a second
   #         'sys',
