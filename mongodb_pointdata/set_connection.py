73abfdaa1553043d24b35f0f7c6b955dd7f08379
Aleena N A <77637415+Aleena25@users.noreply.github.com>
2021-09-04 14:55:27 +0530

    Delete mongodb_pointdata directory

--- mongodb_pointdata/set_connection.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-class Connect(object):
-    @staticmethod    
-    def get_connection():
-        conn = MongoClient("mongodb+srv://Tech27_QtTest:QtTest12345678@cluster0.bajrq.mongodb.net/test")
-        db = conn["RadarDatabase"]
-        # print("Successfully connected")
-        return db
