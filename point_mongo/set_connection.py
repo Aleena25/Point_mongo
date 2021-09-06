0558561119cadef9588d52374e7d3e29daed2976
Aleena N A <77637415+Aleena25@users.noreply.github.com>
2021-09-06 09:51:01 +0530

    Add files via upload

--- point_mongo/set_connection.py
+++ point_mongo/set_connection.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 class Connect(object):
     @staticmethod    
     def get_connection():
-        conn = MongoClient("mongodb+srv://Tech27_QtTest:QtTest12345678@cluster0.bajrq.mongodb.net/test")
+        conn = MongoClient("link for connection")
         db = conn["RadarDatabase"]
         # print("Successfully connected")
         return db
